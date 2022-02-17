62b1d1b1cd51fd42973c5780c73bd9f14547ab3a
rawbin.zhang <rawbin.zhang@lunarax.io>
2022-02-17 23:14:53 +0800

    feat: 增加进一步的复现demo

--- src/pages/debug-svg-print/MonitorInfo.tsx
+++ src/pages/debug-svg-print/MonitorInfo.tsx
@@ -3,7 +3,7 @@ import type { ComponentProps } from 'react';
 import { useEffect, useState } from 'react';
 import { Line } from '@ant-design/plots';
 
-const LineChart: React.FC = (props: ComponentProps<any>) => {
+export const LineChart: React.FC = (props: ComponentProps<any>) => {
   const {
     xField = '',
     yField = '',
@@ -77,7 +77,76 @@ const MonitorInfo: React.FC = ({ params, updatingTimer, localData }) => {
   //     )
   //   )
 
+  // 如下代码可以让svg图像在打印预览的时候显示出来，PDF打印也就能出来，但没有响应式效果
+  // return (
+  //   <List
+  //     grid={{
+  //       gutter: 16,
+  //       xs: 1,
+  //       sm: 1,
+  //       md: 2,
+  //       lg: 2,
+  //       xl: 2,
+  //       xxl: 3,
+  //     }}
+  //   >
+  //     {
+  //       chartData.map(item => {
+  //                 const {chartType, title} = item;
+  //                 const targetFC = chartTypeFCMap[chartType];
+  //                 // @ts-ignore
+  //                 targetFC.key = title;
+  //                 if (targetFC) {
+  //                   return (
+  //                     <List.Item>
+  //                     <Card title={title}>
+  //                       {typeof targetFC === 'function' && (targetFC as Function)(item)}
+  //                     </Card>
+  //                       </List.Item>
+  //                   );
+  //                 }
+  //                 return null
+  //               }
+  //             )
+  //     }
+  //   </List>
+  // );
+
   // 以下代码svg 图形在Chrome打印预览无法看到图片，导出PDF就没有图片
+  // return (
+  //   <List
+  //     grid={{
+  //       gutter: 16,
+  //       xs: 1,
+  //       sm: 1,
+  //       md: 2,
+  //       lg: 2,
+  //       xl: 2,
+  //       xxl: 3,
+  //     }}
+  //     dataSource={chartData}
+  //     renderItem={(item) => {
+  //       const { chartType, title } = item;
+  //       const targetFC = chartTypeFCMap[chartType];
+  //       // @ts-ignore
+  //       targetFC.key = title;
+  //       if (targetFC) {
+  //         return (
+  //           <List.Item>
+  //             <Card title={title}>
+  //               {typeof targetFC === 'function' && (targetFC as Function)(item)}
+  //             </Card>
+  //           </List.Item>
+  //         );
+  //       } else {
+  //         console.error('暂不支持的图表类型:', chartType);
+  //         return null;
+  //       }
+  //     }}
+  //   />
+  // );
+
+  // 如下代码可以让svg图像在打印预览的时候显示出来，PDF打印也就能出来
   return (
     <List
       grid={{
@@ -89,24 +158,28 @@ const MonitorInfo: React.FC = ({ params, updatingTimer, localData }) => {
         xl: 2,
         xxl: 3,
       }}
-      dataSource={chartData}
-      renderItem={(item) => {
+      dataSource={chartData.map(item => {
         const { chartType, title } = item;
         const targetFC = chartTypeFCMap[chartType];
         // @ts-ignore
         targetFC.key = title;
         if (targetFC) {
           return (
-            <List.Item>
-              <Card title={title}>
-                {typeof targetFC === 'function' && (targetFC as Function)(item)}
-              </Card>
-            </List.Item>
+                typeof targetFC === 'function' && (targetFC as Function)(item)
           );
         } else {
           console.error('暂不支持的图表类型:', chartType);
           return null;
         }
+      })}
+      renderItem={(item) => {
+          return (
+            <List.Item>
+              <Card>
+                {item}
+              </Card>
+            </List.Item>
+          );
       }}
     />
   );
--- src/pages/debug-svg-print/PreviewPrintTest.tsx
+++ src/pages/debug-svg-print/PreviewPrintTest.tsx
@@ -2,13 +2,77 @@ import MonitorInfo from './MonitorInfo';
 import React from 'react';
 import localData from './preview-print-test-data';
 import DemoLine from "@/pages/DemoLine";
+import {Card, List} from "antd";
+import {LineChart} from "./MonitorInfo";
 
 const ReportDetail: React.FC = () => {
   console.log(localData);
   return (
     <div id={'report-preview'}>
-      <DemoLine></DemoLine>
       <MonitorInfo localData={localData} />
+      // 如下这种写法无法打印预览svg图
+      <List
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 1,
+          md: 2,
+          lg: 2,
+          xl: 2,
+          xxl: 3,
+        }}
+        dataSource={localData}
+        renderItem={(item) => {
+          return <List.Item>
+            <Card>
+              {LineChart(item)}
+            </Card>
+          </List.Item>
+        }}
+      ></List>
+      // 如下这种写法可以打印预览svg图
+      <List
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 1,
+          md: 2,
+          lg: 2,
+          xl: 2,
+          xxl: 3,
+        }}
+        dataSource={localData.map(item => LineChart(item))}
+        renderItem={(item) => {
+          return <List.Item>
+            <Card>
+              {item}
+            </Card>
+          </List.Item>
+        }}
+      ></List>
+
+      <DemoLine></DemoLine>
+      <List
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 1,
+          md: 2,
+          lg: 2,
+          xl: 2,
+          xxl: 3,
+        }}
+        dataSource={[<DemoLine></DemoLine>,<DemoLine></DemoLine>]}
+        renderItem={(item) => {
+            return <List.Item>
+              <Card>
+                {item}
+              </Card>
+            </List.Item>
+        }}
+      >
+
+      </List>
     </div>
   );
 };
