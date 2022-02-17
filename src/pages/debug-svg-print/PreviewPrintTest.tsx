5545210557e336405ec02ae42e6524b699c97ed8
rawbin.zhang <rawbin.zhang@lunarax.io>
2022-02-17 16:01:29 +0800

    feat: 添加DemoLine对比

--- src/pages/debug-svg-print/PreviewPrintTest.tsx
+++ src/pages/debug-svg-print/PreviewPrintTest.tsx
@@ -1,11 +1,13 @@
 import MonitorInfo from './MonitorInfo';
 import React from 'react';
 import localData from './preview-print-test-data';
+import DemoLine from "@/pages/DemoLine";
 
 const ReportDetail: React.FC = () => {
   console.log(localData);
   return (
     <div id={'report-preview'}>
+      <DemoLine></DemoLine>
       <MonitorInfo localData={localData} />
     </div>
   );
